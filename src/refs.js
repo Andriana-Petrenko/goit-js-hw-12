a6f2b55e6c9754497b97bb4d1f9595fcb43a2ece
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-02-10 01:14:24 +0200

    structure done

--- src/refs.js
+++
@@ -1,7 +0,0 @@
-export const refs = {
-    form: document.querySelector('form'),
-    list: document.querySelector('.gallery'),
-    spanLoader: document.querySelector('.loader'),
-    loadButton: document.querySelector('.load-button'),
-    input: document.querySelector('.form-input')
-}
