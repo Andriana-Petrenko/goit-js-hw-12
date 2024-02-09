59e26ab00505860c9f1b115402ccbe9f8ee727d7
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-02-09 16:35:33 +0200

    task done

--- src/main.js
+++ src/main.js
@@ -17,7 +17,13 @@ let page = 1;
 let inputSearch = '';
 let perPage = 15;
 
+// ----Add an event handler for the "Search" button----
 refs.form.addEventListener('submit', onSearchButton);
+
+// ----Add an event handler for the "Load more" button----
+refs.loadButton.addEventListener('click', onLoadMoreButton)
+
+
 // ----Event Searching photos----
 async function onSearchButton(e){
     e.preventDefault();
@@ -25,60 +31,61 @@ async function onSearchButton(e){
     refs.list.innerHTML = '';
     refs.loadButton.classList.add('hidden');
     if (inputSearch === '') {
-       return iziToast.error({
-        messageColor: '#FFF',
-        color: '#EF4040',
-        iconUrl: closeIcon,
-        position: 'topRight',
-        message: 'Please,enter what do you want to find!',
+        return iziToast.error({
+            messageColor: '#FFF',
+            color: '#EF4040',
+            iconUrl: closeIcon,
+            position: 'topRight',
+            message: 'Please,enter what do you want to find!',
         });
     };
     page = 1;
     try {
-        const { hits, totalHits } = await fetchPhotos();
+        const { hits, totalHits } = await getPhotos();
+        noPhotos(hits);
         renderPhoto(hits);
         addLoadButton(totalHits);
-        }
+    }
     catch (error) {
         iziToast.error({
-                messageColor: '#FFF',
-                color: '#EF4040',
-                iconUrl: closeIcon,
-                position: 'topRight',
-                message: `${error}`,
-            })
+            messageColor: '#FFF',
+            color: '#EF4040',
+            iconUrl: closeIcon,
+            position: 'topRight',
+            message: `${error}`,
+        })
     } 
     simpleLightbox();
     refs.form.reset(); 
 }
 
-// ----Promise function----
-async function fetchPhotos() {
+// ----Request function----
+async function getPhotos() {
     refs.spanLoader.classList.remove('hidden');
     const response = await axios.get( 'https://pixabay.com/api/',{
-      params: {
-    key: "42112521-3ff4dfc201bab0977369cd2bc",
-    q: `${inputSearch}`,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: "true",
-    per_page: perPage,
-    page: page},
+        params: {
+            key: "42112521-3ff4dfc201bab0977369cd2bc",
+            q: `${inputSearch}`,
+            image_type: "photo",
+            orientation: "horizontal",
+            safesearch: "true",
+            per_page: perPage,
+            page: page},
     });
     const {hits, totalHits } = response.data;
     refs.spanLoader.classList.add('hidden');
     return {hits, totalHits};
 }
 
-// ----Add an event handler for the "Load more" button----
-refs.loadButton.addEventListener('click', async () => {  
+// ----Event Loading photos----
+ async function onLoadMoreButton() {  
     page++;
     try {
-        const {hits, totalHits} = await fetchPhotos();
+        const {hits, totalHits} = await getPhotos();
         renderPhoto(hits);
-        smoothScroll();
-        noMoreLoadingPhotos(page,totalHits);
-    } catch (error) {
+        endOfCollection(page, totalHits);
+    }
+    catch (error) {
         console.log(error);
         iziToast.error({
                 messageColor: '#FFF',
@@ -87,33 +94,35 @@ refs.loadButton.addEventListener('click', async () => {
                 position: 'topRight',
                 message: `${error}`,
             })
-    };
+     };
+    smoothScroll();
     simpleLightbox();
-})
+}
+
 
 // ----Markup HTML----
 function renderPhoto(hits) {
     const markup = hits
         .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) =>
- `<li class='gallery-item'>
-  <a class='gallery-link' href='${largeImageURL}'>
-    <img class='gallery-image' src='${webformatURL}' alt='${tags}'/>
-  </a>
-<div class='container-app'>
-<p><span>Likes</span> ${likes}</p>
-<p><span>Views</span> ${views}</p>
-<p><span>Comments</span> ${comments}</p>
-<p><span>Downloads</span> ${downloads}</p>
-</div>
- </li>`)
+            `<li class='gallery-item'>
+             <a class='gallery-link' href='${largeImageURL}'>
+               <img class='gallery-image' src='${webformatURL}' alt='${tags}'/>
+             </a>
+             <div class='container-app'>
+              <p><span>Likes</span> ${likes}</p>
+              <p><span>Views</span> ${views}</p>
+              <p><span>Comments</span> ${comments}</p>
+              <p><span>Downloads</span> ${downloads}</p>
+             </div>
+            </li>`)
         .join('');
     refs.list.insertAdjacentHTML('beforeend', markup);
 }
 
-// ----smoothScroll----
+// ----SmoothScroll----
 function smoothScroll() {
- const {height: itemHeight} = document.querySelector('.gallery-item').getBoundingClientRect();
-  window.scrollBy({
+    const { height: itemHeight } = document.querySelector('.gallery-item').getBoundingClientRect();
+    window.scrollBy({
     top: itemHeight*2,
     behavior: 'smooth',
   });
@@ -125,7 +134,13 @@ function addLoadButton(totalHits) {
     if (totalPages>1) {
        refs.loadButton.classList.remove('hidden'); 
     };
-    if (totalHits === 0) {
+     
+}
+
+// ----No request photos---- 
+function noPhotos(hits){
+    if (hits.length === 0) {
+        console.log('hello');
         iziToast.error({
             messageColor: '#FFF',
             color: '#EF4040',
@@ -133,12 +148,11 @@ function addLoadButton(totalHits) {
             position: 'topRight',
             message: 'Sorry, there are no images matching your search query. Please try again!',
         });
-        };  
+        }; 
 }
 
-
 // ---- Check the end of the collection----
-function noMoreLoadingPhotos(page, totalHits) {
+function endOfCollection(page, totalHits) {
     const totalPages = Math.ceil(totalHits / perPage);
     if (page >totalPages) {
         refs.loadButton.classList.add('hidden');
@@ -152,7 +166,7 @@ function noMoreLoadingPhotos(page, totalHits) {
   }   
 }
 
-// ----library simpleLightbox----
+// ----Library SimpleLightbox----
 function simpleLightbox(){
     let gallery = new SimpleLightbox('.gallery a',{
     captionsData: 'alt',
